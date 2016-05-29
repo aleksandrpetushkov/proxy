eb56eeb7db42291f922deb6e2663bb46d7f69b78
Alexander Petushkov <aleks.petushkov@hotmail.com>
2016-05-30 02:24:36 +0600

    0.0.4

--- proxy/proxy/main.cpp
+++ proxy/proxy/main.cpp
@@ -1,14 +1,12 @@
-#include "libbitmap.cpp"
+//#include "libbitmap.cpp"
 #include <iostream>
+#include "Proxy.h"
 
 using namespace std;
 
 int main()
 {
-	Bitmap bmp;
-	bmp.load("123.bmp");
-	cout << bmp.m_height;
-	cout << bmp.m_width;
+	cout << Proxy::get_size("kkk_1010xddddd");
 
 
 
